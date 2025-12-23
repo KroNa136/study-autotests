84e9f0da00b9066e6b8028dc341e4fee645fe681
KroNa136 <alina.tsybina.2002@yandex.ru>
2025-12-23 21:40:28 +0300

    Defined test functions

--- tests/test_positive.py
+++ tests/test_positive.py
@@ -3,21 +3,22 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from feedback_page import FeedbackPage
 
-options = Options()
-options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
+def test_positive_case():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
 
-driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
 
-try:
-    feedbackPage = FeedbackPage(driver)
-    feedbackPage.open()
-    feedbackPage.send_feedback("Alice", "alice@gmail.com", "Some message for the feedback form.")
-    message = feedbackPage.get_success_message_text()
-    print(message)
-except TimeoutException:
-    print("timeout")
-finally:
-    driver.quit()
+    try:
+        feedback_page = FeedbackPage(driver)
+        feedback_page.open()
+        feedback_page.send_feedback("Alice", "alice@gmail.com", "Some message for the feedback form.")
+        message = feedback_page.get_success_message_text()
+        print(message)
+    except TimeoutException:
+        print("timeout")
+    finally:
+        driver.quit()
